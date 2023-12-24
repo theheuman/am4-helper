36ee8a0a993ea2090bd10e7325c684eed8802623
TheHeuman <jacob.heuman@modea.com>
2023-12-24 15:04:03 -0500

    start

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import {Preferences} from "@capacitor/preferences";
+import {NgIf} from "@angular/common";
+
+const storageKey = 'dayStart';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, NgIf],
 })
 export class Tab1Page {
-  constructor() {}
+  startedAt: string;
+
+  constructor() {
+    this.startedAt = '';
+    Preferences.get({
+      key: storageKey
+    }).then((result) => {
+      const milliseconds = Number(result.value)
+      if (milliseconds) {
+        this.startedAt = this.convertDate(new Date(milliseconds));
+      }
+    })
+  }
+
+  startDay() {
+    const now = Date.now();
+    const nowDate = new Date(now)
+
+    Preferences.set({
+      key: storageKey,
+      value: now + '',
+    }).then(() => {
+      console.log("Day started!", nowDate)
+      this.startedAt = this.convertDate(nowDate);
+    })
+  }
+
+  reset() {
+    Preferences.remove({
+      key: storageKey,
+    }).then(() => {
+      console.log("Day reset!", )
+      this.startedAt = '';
+    })
+  }
+
+  convertDate(date: Date) {
+    return date.toLocaleString('default', { hour: '2-digit', minute: 'numeric', hour12: false });
+  }
 }
